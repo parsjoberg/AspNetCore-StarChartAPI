c6938705ccf478a5052b6932d9183814934f4ba3
Pär Sjöberg <paer.sjoberg@googlemail.com>
2021-12-14 20:10:53 +0100

    module 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,45 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.SingleOrDefault(co => co.Id == id);
+            if (celestialObject == null)
+                return NotFound();            
+            celestialObject.Satellites = _context.CelestialObjects.Where(co => co.OrbitedObjectId == id).ToList();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(co => co.Name == name).ToList();
+            if (celestialObjects == null || celestialObjects.Count == 0)
+                return NotFound();
+
+            celestialObjects.ForEach(celestialObject =>
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(co => co.OrbitedObjectId == celestialObject.Id).ToList();
+            });            
+
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            celestialObjects.ForEach(celestialObject =>
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(co => co.OrbitedObjectId == celestialObject.Id).ToList();
+            });            
+
+            return Ok(celestialObjects);
         }
     }
 }
